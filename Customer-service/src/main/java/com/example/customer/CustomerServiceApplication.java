695ed721858a365ff0e134bf7f7f36a5fbc796f1
babai-abhishek <babbi.abhishek@gmail.com>
2022-06-21 17:10:05 +0530

    code updated for tomcat POC

--- Customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
+++ Customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
@@ -2,9 +2,16 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class CustomerServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CustomerServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
